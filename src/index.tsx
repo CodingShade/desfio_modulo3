91f02222985de223678b7b5db630826ffd366dee
Thomas O. Bender <145614085+CodingShade@users.noreply.github.com>
2025-03-04 06:28:39 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Importe createRoot
+import { createRoot } from 'react-dom/client'; 
 import App from './App';
 import './index.css';
 
-// Crie a raiz do aplicativo
-const container = document.getElementById('root');
-const root = createRoot(container!); // Use createRoot
 
-// Renderize o aplicativo
+const container = document.getElementById('root');
+const root = createRoot(container!); 
+
+
 root.render(
   <React.StrictMode>
     <App />
